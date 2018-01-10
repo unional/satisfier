c243909291e28bf6c8a231a0e9673bba7fd1559a
unional <homawong@gmail.com>
2018-01-09 17:52:08 -0800

    feat: add CallEntry.tersify()
    BREAKING CHANGE:
    renamed CallRecord to CallEntry
    CallRecord represents  a serializable record.
    renamed CallEntry properties from arguemnts and result to inputs, output to that it works better with spread operator (because arguments is reserved).

--- src/createCallRecordCreator.ts
+++ src/createCallRecordCreator.ts
@@ -1,4 +1,6 @@
-import { CallRecord } from './interfaces'
+import { tersify, tersible } from 'tersify'
+
+import { CallEntry } from './interfaces'
 
 export function createCallRecordCreator(args: any[]) {
   let resolve
@@ -7,12 +9,42 @@ export function createCallRecordCreator(args: any[]) {
     resolve = a
     reject = r
   })
+  const callEntry = Object.assign(p, {
+    inputs: args,
+    getCallRecord() {
+      return callEntry.then(asyncOutput => {
+        const { inputs, output, error } = callEntry
+        return tersible({
+          inputs,
+          output,
+          error,
+          asyncOutput
+        }, () => tersify({
+          inputs,
+          output,
+          error,
+          asyncOutput
+        }, { maxLength: Infinity }))
+      }, asyncError => {
+        const { inputs, output, error } = callEntry
+        return tersible({
+          inputs,
+          output,
+          error,
+          asyncError
+        }, () => tersify({
+          inputs,
+          output,
+          error,
+          asyncError
+        }, { maxLength: Infinity }))
+      })
+    }
+  }) as CallEntry
 
   return {
     resolve,
     reject,
-    callRecord: Object.assign(p, {
-      arguments: args
-    }) as CallRecord
+    callEntry
   }
 }
--- src/spy.spec.ts
+++ src/spy.spec.ts
@@ -11,8 +11,8 @@ test('record argument and result', t => {
 
   t.is(calls.length, 1)
   const cr = calls[0]
-  t.is(cr.arguments[0], 1)
-  t.is(cr.result, 2)
+  t.is(cr.inputs[0], 1)
+  t.is(cr.output, 2)
 })
 
 function throws() { throw new Error('thrown') }
@@ -26,30 +26,39 @@ test('capture error', t => {
   t.is(calls[0].error, err)
 })
 
+test('tersify for sync call', async t => {
+  const { fn, calls } = spy(increment)
+
+  t.is(fn(1), 2)
+  const record = await calls[0].getCallRecord()
+  t.is(record.tersify(), `{ inputs: [1], output: 2, error: undefined, asyncOutput: undefined }`)
+})
+
 // this is not a valid test as the package is used for boundary testing.
 // Boundary function are not expected to make changes to the arguments
 test.skip('argument should be immutable', t => {
   function mutate(x) { x.a++ }
   const { fn, calls } = spy(mutate)
   fn({ a: 1 })
-  const cr = calls[0]
-  t.is(cr.arguments[0].a, 1)
+  const entry = calls[0]
+  t.is(entry.inputs[0].a, 1)
 })
 
 function invoke(x, cb) { cb(x) }
 
-test('callback are spied', t => {
+test('callback are spied', async t => {
   const { fn, calls } = spy(invoke)
   fn(1, x => t.is(x, 1))
-  const cr = calls[0]
-  t.is(cr.arguments[0], 1)
-  return cr.then(r => t.deepEqual(r, [1]))
+  const entry = calls[0]
+  t.is(entry.inputs[0], 1)
+  return entry.then(x => t.deepEqual(x, [1]))
 })
 
 function callbackOnLiterial(options) {
   options.success(++options.data)
 }
-test('spec on jquery style callback', t => {
+
+test('spec on jquery style callback', async t => {
   const { fn, calls } = spy(callbackOnLiterial)
   fn({
     data: 1,
@@ -58,18 +67,18 @@ test('spec on jquery style callback', t => {
     }
   })
 
-  const call = calls[0]
-  return call.then(response => t.is(response[0], 2))
+  const output = await calls[0]
+  t.is(output[0], 2)
 })
 
 
 const resolve = x => Promise.resolve(x)
 
-test('then() will receive result from promise', t => {
+test('then() will receive result from promise', async t => {
   const { fn, calls } = spy(resolve)
   // tslint:disable-next-line
   fn(1)
-  return calls[0].then(x => t.is(x, 1))
+  t.is(await calls[0], 1)
 })
 
 test('result from promise can be retrieved from await on the call', async t => {
