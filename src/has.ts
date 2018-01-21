ed5c0eac06eb57b0e0ee8dada6964e893a916a1c
Homa Wong <homawong@gmail.com>
2018-01-20 19:47:26 -0800

    Add doc to has()

--- src/has.ts
+++ src/has.ts
@@ -7,6 +7,10 @@ import {
 
 import { createSatisfier } from './createSatisfier'
 
+/**
+ * Check if an array have at least one entry satisfying the expectation.
+ * @param expectation expectation
+ */
 export function has(expectation) {
   const s = createSatisfier(expectation)
   return tersible(e => e && Array.isArray(e) && e.some(v => s.test(v)), () => `has(${tersify(expectation)})`)
