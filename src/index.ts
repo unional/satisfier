cb7729119e8aa33d35b8800871e098886f8803a2
Homa Wong <homawong@gmail.com>
2018-01-20 22:13:20 -0800

    chore: ignore `has()` from coverage

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,8 @@ export * from './isTypeOf'
 // @ts-ignore
 import { Tersify } from 'tersify'
 import { some } from './some'
+
+// istanbul ignore next
 export function has(expectation) {
   console.warn('has is deprecated, please use some().')
   return some(expectation)
